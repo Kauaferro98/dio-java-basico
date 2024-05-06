c1d7fef5d228d61c02874a07a956d2431cd581e3
Kauã Ferro <kaua.ferro@icloud.com>
2024-05-06 18:12:51 -0300

    variavel

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,6 +1,8 @@
 public class MinhaClasse {
     public static void main(String[] args) {
         String nome = "kauã";
+        int dataNascimento = 28;
+        boolean verdadeiro = false;
         System.out.println(nome);
     }
 }
