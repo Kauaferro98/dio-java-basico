06dba8042552ee0f0040884b55661fb479377744
Kauã Ferro <kaua.ferro@icloud.com>
2024-05-06 18:20:13 -0300

    método

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,8 +1,15 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        String nome = "kauã";
+        String primeiroNome = "kauã";
+        String segundoNome = "ferro";
         int dataNascimento = 28;
         boolean verdadeiro = false;
-        System.out.println(nome);
+
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+        System.out.println(nomeCompleto);
+       
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método" + " " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
