cb4f810a1ba0f9b10ccaed3f7dc5ff2174b97570
Kauã Ferro <kaua.ferro@icloud.com>
2024-05-14 17:17:05 -0300

    identação

--- java-anatomia-classes/src/BoletimEstudantil.java
+++ java-anatomia-classes/src/BoletimEstudantil.java
@@ -2,10 +2,10 @@ public class BoletimEstudantil {
     public static void main(String[] args) {
         int mediaFinal = 6;
         if(mediaFinal<6)
-        System.out.println("REPROVADO");
+            System.out.println("REPROVADO");
         else if (mediaFinal==6)
-        System.out.println("PROVA MINERVA ");
+            System.out.println("PROVA MINERVA ");
         else
-        System.out.println("APROVADO");
+            System.out.println("APROVADO");
     }
 }
